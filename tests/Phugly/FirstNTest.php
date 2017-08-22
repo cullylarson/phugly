8c8bc286d2a0dd524b1b6fcf4eaf91421ecd90e2
Cully Larson <cully.larson@gmail.com>
2017-08-22 14:42:12 -0700

    Had mixed up the filenames of FirstNTeset and RestTest (they were testing the opposide function).

--- tests/Phugly/FirstNTest.php
+++ tests/Phugly/FirstNTest.php
@@ -2,69 +2,69 @@
 
 use PHPUnit\Framework\TestCase;
 use Phugly as F;
-use function Phugly\rest;
+use function Phugly\firstN;
 
-class RestTest extends TestCase {
+class FirstNTest extends TestCase {
     public function testNumericIndex() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(rest(2, $x), [2 => 'c']);
+        $this->assertEquals(firstN(2, $x), ['a', 'b']);
     }
 
     public function testNumericIndexesPreserved() {
         $x = [0 => 'a', 5 => 'b', 6 => 'c'];
 
-        $this->assertEquals(rest(2, $x), [6 => 'c']);
+        $this->assertEquals(firstN(2, $x), [0 => 'a', 5 => 'b']);
     }
 
     public function testNumericIndexesPreservedOutOfOrder() {
         $x = [100 => 'a', 15 => 'b', 6 => 'c'];
 
-        $this->assertEquals(rest(2, $x), [6 => 'c']);
+        $this->assertEquals(firstN(2, $x), [100 => 'a', 15 => 'b']);
     }
 
     public function testAssoc() {
         $x = ['a' => 100, 'b' => 200, 'c' => 300];
 
-        $this->assertEquals(rest(2, $x), ['c' => 300]);
+        $this->assertEquals(firstN(2, $x), ['a' => 100, 'b' => 200]);
     }
 
     public function testOutOfRange() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(rest(5, $x), []);
+        $this->assertEquals(firstN(5, $x), ['a', 'b', 'c']);
     }
 
     public function testEmpty() {
         $x = [];
 
-        $this->assertEquals(rest(5, $x), []);
+        $this->assertEquals(firstN(5, $x), []);
     }
 
     public function testNZero() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(rest(0, $x), ['a', 'b', 'c']);
+        $this->assertEquals(firstN(0, $x), []);
     }
 
     public function testImmutable() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(rest(2, $x), [2 => 'c']);
+        $this->assertEquals(firstN(2, $x), ['a', 'b']);
         $this->assertEquals($x, ['a', 'b', 'c']);
     }
 
     public function testCurried() {
-        $afterOne = rest(1);
+        $firstTwo = firstN(2);
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals($afterOne($x), [1 => 'b', 2 => 'c']);
+        $this->assertEquals($firstTwo($x), ['a', 'b']);
     }
 
     public function testStatic() {
-        $rest = F\rest;
+        $firstN = F\firstN;
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals($rest(2, $x), [2 => 'c']);
+        $this->assertEquals($firstN(2, $x), ['a', 'b']);
     }
 }
--- tests/Phugly/RestTest.php
+++ tests/Phugly/RestTest.php
@@ -2,69 +2,69 @@
 
 use PHPUnit\Framework\TestCase;
 use Phugly as F;
-use function Phugly\firstN;
+use function Phugly\rest;
 
-class FirstNTest extends TestCase {
+class RestTest extends TestCase {
     public function testNumericIndex() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(firstN(2, $x), ['a', 'b']);
+        $this->assertEquals(rest(2, $x), [2 => 'c']);
     }
 
     public function testNumericIndexesPreserved() {
         $x = [0 => 'a', 5 => 'b', 6 => 'c'];
 
-        $this->assertEquals(firstN(2, $x), [0 => 'a', 5 => 'b']);
+        $this->assertEquals(rest(2, $x), [6 => 'c']);
     }
 
     public function testNumericIndexesPreservedOutOfOrder() {
         $x = [100 => 'a', 15 => 'b', 6 => 'c'];
 
-        $this->assertEquals(firstN(2, $x), [100 => 'a', 15 => 'b']);
+        $this->assertEquals(rest(2, $x), [6 => 'c']);
     }
 
     public function testAssoc() {
         $x = ['a' => 100, 'b' => 200, 'c' => 300];
 
-        $this->assertEquals(firstN(2, $x), ['a' => 100, 'b' => 200]);
+        $this->assertEquals(rest(2, $x), ['c' => 300]);
     }
 
     public function testOutOfRange() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(firstN(5, $x), ['a', 'b', 'c']);
+        $this->assertEquals(rest(5, $x), []);
     }
 
     public function testEmpty() {
         $x = [];
 
-        $this->assertEquals(firstN(5, $x), []);
+        $this->assertEquals(rest(5, $x), []);
     }
 
     public function testNZero() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(firstN(0, $x), []);
+        $this->assertEquals(rest(0, $x), ['a', 'b', 'c']);
     }
 
     public function testImmutable() {
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals(firstN(2, $x), ['a', 'b']);
+        $this->assertEquals(rest(2, $x), [2 => 'c']);
         $this->assertEquals($x, ['a', 'b', 'c']);
     }
 
     public function testCurried() {
-        $firstTwo = firstN(2);
+        $afterOne = rest(1);
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals($firstTwo($x), ['a', 'b']);
+        $this->assertEquals($afterOne($x), [1 => 'b', 2 => 'c']);
     }
 
     public function testStatic() {
-        $firstN = F\firstN;
+        $rest = F\rest;
         $x = ['a', 'b', 'c'];
 
-        $this->assertEquals($firstN(2, $x), ['a', 'b']);
+        $this->assertEquals($rest(2, $x), [2 => 'c']);
     }
 }
